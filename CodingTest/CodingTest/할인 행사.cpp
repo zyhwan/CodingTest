29e3d3ccb0875cf2c68392bb8aa4c7cc5d322115
zyhwan <145274494+zyhwan@users.noreply.github.com>
2026-01-26 22:02:18 +0900

    Create 할인 행사.cpp

---
+++ CodingTest/CodingTest/할인 행사.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int solution(vector<string> want, vector<int> number, vector<string> discount) {
+    int answer = 0;
+
+
+
+    return answer;
+}
